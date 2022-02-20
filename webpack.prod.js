3b903fce32c0c80d85df62d44f9bd68ab45d90af
Alberto Aguado <a.aguadochacon@gmail.com>
2022-02-20 23:29:20 +0100

    update from weather journal app project

--- webpack.prod.js
+++ webpack.prod.js
@@ -10,39 +10,35 @@ const WorkboxPlugin = require('workbox-webpack-plugin');
 module.exports = {
     entry: './src/client/index.js',
     output: {
-        // name of generated Javascript output file inserted (by the plugin 'HtmlWebPackPlugin') in the 'head' section of index.html
         filename: 'bundle.js',
-        // folder where 'bundle.js' will be placed
         path: path.resolve(__dirname, './dist')
     },
     mode: 'production',
     module: {
         rules: [
-            // CSS loader configuration; use 'MiniCssExtractPlugin' plugin to extract CSS into separate files
+         
             {
                 test: /\.css$/,
                 use: [
                     MiniCssExtractPlugin.loader, 'css-loader'
                 ]
             },
-            // SCSS (SASS) loader configuration
+
             {
                 test: /\.scss$/,
                 use: [
                     MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
                 ]
             },
-            // Javascript loader configuration with transpiler options
-            // NOTE: in production build mode, webpack automatically minimizes JS files 
+
             {
                 test: '/\.js$/',
                 exclude: /node_modules/,
-                // babel loader configuration for ES[6/7/8] transpiling
+
                 use: {
                     loader: "babel-loader",
                     options: {
-                        // transpiles Javascript code to cross-browser compatible (ES5) code
-                        // presets: [ '@babel/env' ]
+  
                         presets: ['@babel/preset-env']
                     }
                 },
@@ -50,7 +46,7 @@ module.exports = {
         ]
     },
     optimization: {
-        // minimize/compress/optimize the generated CSS code
+
         minimizer: [
             new CssMinimizerPlugin()
         ],
@@ -59,24 +55,22 @@ module.exports = {
         new MiniCssExtractPlugin({ filename: '[name].scss' }),
         new HtmlWebPackPlugin({
             template: "src/client/views/index.html",
-            // filename: "./index.html"
+  
             filename: "index.html"
         }),
         new CleanWebpackPlugin({
-            // Simulate the removal of files
+        
             dry: true,
-            // Write Logs to Console
+        
             verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
+         
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
         }),
         new DotEnv(),
-        // generate the service worker file
-        // NOTE: use this plugin only in production mode - Service Worker requires HTTPS/secure connection
+
         new WorkboxPlugin.GenerateSW({
-            // these options encourage the ServiceWorkers to get in there fast
-            // and not allow any straggling "old" SWs to hang around
+
             clientsClaim: true,
             skipWaiting: true,
         }),
